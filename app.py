f90c6b5293e564335bd75e9bf0770adf3445ff08
Rakhmatullaev Abdulaziz <frrhyk@gmail.com>
2024-05-08 15:11:50 +0500

    Became the most SECURE

--- app.py
+++ app.py
@@ -37,6 +37,15 @@ with app.app_context():
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 
+def is_face(img):
+    try:
+        DeepFace.analyze(img, actions=["gender"])
+        return True
+    except ValueError as e:
+        print("Face could not be detected:", e)
+        return False
+
+
 def check_face(image_data, username):
     try:
         user = User.query.filter_by(username=username).first()
@@ -62,10 +71,19 @@ def check_face(image_data, username):
 def index():
     if "user_id" in session:
         user = User.query.get(session["user_id"])
+
+        # Decode the image data from base64
+        img_data = user.image.split(",")[1]
+        img_binary = base64.b64decode(img_data)
+
+        # Create a data URI for the image
+        img_uri = "data:image/jpg;base64," + base64.b64encode(img_binary).decode()
+
         return render_template(
             "index.html",
             username=user.username,
             gender=user.gender,
+            image=img_uri,
             check_in=user.check_in,
             check_out=user.check_out,
         )
@@ -81,11 +99,14 @@ def login():
 
         user = User.query.filter_by(username=username).first()
         if user:
-            if check_face(image_data, username):
-                session["user_id"] = user.id
-                return jsonify({"is_match": True})
+            if is_face(image_data):
+                if check_face(image_data, username):
+                    session["user_id"] = user.id
+                    return jsonify({"is_match": True})
+                else:
+                    return jsonify({"is_match": False})
             else:
-                return jsonify({"is_match": False})
+                return jsonify({"no_face": True})
         else:
             return jsonify({"user_not_found": True})
     else:
@@ -110,40 +131,33 @@ def register():
 
         if not User.query.filter_by(username=username).first():
 
-            new_user = User(
-                username=username,
-                image=image_data,
-                check_in=check_in,
-                check_out=check_out,
-            )
+            if is_face(image_data):
+                new_user = User(
+                    username=username,
+                    image=image_data,
+                    check_in=check_in,
+                    check_out=check_out,
+                )
 
-            db.session.add(new_user)
-            db.session.commit()
+                db.session.add(new_user)
+                db.session.commit()
 
-            reference_img = new_user.image
-            decoded_data = base64.b64decode(image_data.split(",")[1])
-            nparr = np.frombuffer(decoded_data, np.uint8)
-            frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-            reference_img = base64.b64decode(reference_img.split(",")[1])
-            nparr_ref = np.frombuffer(reference_img, np.uint8)
-            ref_frame = cv2.imdecode(nparr_ref, cv2.IMREAD_COLOR)
+                decoded_data = base64.b64decode(image_data.split(",")[1])
+                nparr = np.frombuffer(decoded_data, np.uint8)
+                frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-            # Ensure both frames are in RGB format
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            ref_frame = cv2.cvtColor(ref_frame, cv2.COLOR_BGR2RGB)
+                gen_inf = DeepFace.analyze(frame, actions=["gender"])
+                if isinstance(gen_inf, list):
+                    gen_inf = gen_inf[0]
 
-            gen_inf = DeepFace.analyze(frame, actions=["gender"])
-            if isinstance(gen_inf, list):
-                gen_inf = gen_inf[0]
+                gender = gen_inf.get("dominant_gender")
 
-            gender = gen_inf.get("dominant_gender")
+                new_user.gender = gender
+                db.session.commit()
 
-            new_user.gender = gender
-            db.session.commit()
-
-
-            return jsonify({"is_available": True})
-
+                return jsonify({"is_available": True})
+            else:
+                return jsonify({"no_face": True})
         else:
             return jsonify({"is_available": False})
     else:
