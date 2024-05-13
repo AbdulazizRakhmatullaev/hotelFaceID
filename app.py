a54ddb6075ab131988f7027afa176a45493a11f1
Rakhmatullaev Abdulaziz <frrhyk@gmail.com>
2024-05-14 04:12:12 +0500

    Admin panel and some bug fixes

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import numpy as np
 from deepface import DeepFace
 from datetime import datetime
 
-# https://github.com/serengil/deepface_models/releases/download/v1.0/gender_model_weights.h5
-
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
 
@@ -23,11 +21,19 @@ db = SQLAlchemy(app)
 # Define User model
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), nullable=False)
-    gender = db.Column(db.String(6), nullable=True)
-    check_in = db.Column(db.DateTime, default=datetime.now)
-    check_out = db.Column(db.DateTime, default=datetime.now)
     image = db.Column(db.Text, nullable=False)
+
+    username = db.Column(db.String(30), nullable=False)
+    first_name = db.Column(db.String(20), nullable=False)
+    last_name = db.Column(db.String(25), nullable=False)
+    dob = db.Column(db.DateTime)
+    passport = db.Column(db.String(9), nullable=False)
+
+    hotel_name = db.Column(db.String, nullable=False)
+    room_num = db.Column(db.Integer, nullable=False)
+
+    check_in = db.Column(db.DateTime)
+    check_out = db.Column(db.DateTime)
 
 
 # Create database tables
@@ -37,6 +43,7 @@ with app.app_context():
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 
+
 def is_face(img):
     try:
         DeepFace.analyze(img, actions=["gender"])
@@ -44,6 +51,7 @@ def is_face(img):
     except ValueError as e:
         print("Face could not be detected:", e)
         return False
+
 
 
 def check_face(image_data, username):
@@ -70,6 +78,7 @@ def check_face(image_data, username):
 @app.route("/")
 def index():
     if "user_id" in session:
+
         user = User.query.get(session["user_id"])
 
         # Decode the image data from base64
@@ -82,13 +91,18 @@ def index():
         return render_template(
             "index.html",
             username=user.username,
-            gender=user.gender,
+            first_name=user.first_name,
+            last_name=user.last_name,
+            dob=user.dob,
+            hotel_name=user.hotel_name,
+            room_num=user.room_num,
+            passport=user.passport,
             image=img_uri,
             check_in=user.check_in,
             check_out=user.check_out,
         )
     else:
-        return redirect(url_for("login"))
+        return redirect(url_for("admin"))
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -101,7 +115,11 @@ def login():
         if user:
             if is_face(image_data):
                 if check_face(image_data, username):
-                    session["user_id"] = user.id
+                    if user.username == "admin":
+                        session["admin"] = user.id
+                    else:
+                        session["user_id"] = user.id
+
                     return jsonify({"is_match": True})
                 else:
                     return jsonify({"is_match": False})
@@ -110,7 +128,7 @@ def login():
         else:
             return jsonify({"user_not_found": True})
     else:
-        if "user_id" in session:
+        if ("user_id" in session) or ("admin" in session):
             print("User already logged in. Redirecting to index.")
             return redirect(url_for("index"))
         else:
@@ -121,19 +139,34 @@ def login():
 def register():
     if request.method == "POST":
         image_data = request.form["image"]
+
         username = request.form["username"]
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        passport = request.form["passport"]
+
+        hotel_name = request.form["hotel_name"]
+        room_num = request.form["room_num"]
+
+        dob_str = request.form["dob"]
         check_in_str = request.form["check_in"]
         check_out_str = request.form["check_out"]
 
         # Convert date strings to Python date objects
-        check_in = datetime.strptime(check_in_str, "%Y-%m-%d")
-        check_out = datetime.strptime(check_out_str, "%Y-%m-%d")
+        dob = datetime.strptime(dob_str, "%Y-%m-%d")
+        check_in = datetime.strptime(check_in_str, "%Y-%m-%dT%H:%M")
+        check_out = datetime.strptime(check_out_str, "%Y-%m-%dT%H:%M")
 
         if not User.query.filter_by(username=username).first():
-
             if is_face(image_data):
                 new_user = User(
                     username=username,
+                    first_name=first_name,
+                    last_name=last_name,
+                    passport=passport,
+                    dob=dob,
+                    hotel_name=hotel_name,
+                    room_num=room_num,
                     image=image_data,
                     check_in=check_in,
                     check_out=check_out,
@@ -142,26 +175,22 @@ def register():
                 db.session.add(new_user)
                 db.session.commit()
 
-                decoded_data = base64.b64decode(image_data.split(",")[1])
-                nparr = np.frombuffer(decoded_data, np.uint8)
-                frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
-                gen_inf = DeepFace.analyze(frame, actions=["gender"])
-                if isinstance(gen_inf, list):
-                    gen_inf = gen_inf[0]
-
-                gender = gen_inf.get("dominant_gender")
-
-                new_user.gender = gender
-                db.session.commit()
-
-                return jsonify({"is_available": True})
+                return jsonify({"user_is_available": True})
             else:
                 return jsonify({"no_face": True})
         else:
-            return jsonify({"is_available": False})
+            return jsonify({"user_is_available": False})
     else:
         return render_template("register.html")
+
+@app.route("/admin", methods=["GET", "POST"])
+def admin():
+    if "admin" in session:
+        users = User.query.filter(User.username!="admin").all()
+
+        return render_template("admin.html", users=users)
+    else:
+        return redirect(url_for("login"))
 
 
 @app.route("/check_face", methods=["POST"])
@@ -176,9 +205,46 @@ def check_face_route():
 
 @app.route("/logout", methods=["POST"])
 def logout():
-    session.pop("user_id", None)
+
+    if "user_id" in session:
+        ses = "user_id"
+    else:
+        ses = "admin"
+
+    session.pop(ses, None)
+
     return redirect(url_for("login"))
 
+@app.route("/myaccount/delete", methods=["POST"])
+def delete_my_acc():
+
+    if request.method == "POST":
+        username = request.form.get("username")
+        user = User.query.filter_by(username=username).first()
+
+        # delete the white boi
+        db.session.delete(user)
+        session.pop("user_id", None)
+
+        db.session.commit()
+
+        redirect(url_for("login"))
+    return redirect(url_for("index"))
+
+# for admins to delete accounts
+@app.route("/admin/account/delete", methods=["POST"])
+def delete_acc():
+
+    if request.method == "POST":
+        username = request.form.get("username")
+        user = User.query.filter_by(username=username).first()
+
+        # delete the white boi
+        db.session.delete(user)
+        db.session.commit()
+
+        redirect(url_for("admin"))
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
